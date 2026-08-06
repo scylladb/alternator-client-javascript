9de15c6082eb1d0557b13281f984d943171cbc32
Dmitry Kropachev <dmitry.kropachev@gmail.com>
2026-08-06 08:30:44 -0400

    Add ScyllaDB license headers

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,42 @@
+/*
+ * Copyright ScyllaDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import js from "@eslint/js";
+import licenseHeaderPlugin from "eslint-plugin-license-header";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
 const tsFiles = ["src/**/*.ts", "test/**/*.ts", "vitest.config.ts", "vitest.integration.config.ts", "tsup.config.ts"];
+const licenseHeader = [
+  "/*",
+  " * Copyright ScyllaDB, Inc.",
+  " *",
+  " * Licensed under the Apache License, Version 2.0 (the \"License\");",
+  " * you may not use this file except in compliance with the License.",
+  " * You may obtain a copy of the License at",
+  " *",
+  " * http://www.apache.org/licenses/LICENSE-2.0",
+  " *",
+  " * Unless required by applicable law or agreed to in writing, software",
+  " * distributed under the License is distributed on an \"AS IS\" BASIS,",
+  " * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.",
+  " * See the License for the specific language governing permissions and",
+  " * limitations under the License.",
+  " */",
+];
 
 export default tseslint.config(
   {
@@ -11,6 +45,15 @@ export default tseslint.config(
       "dist/**",
       "node_modules/**",
     ],
+  },
+  {
+    files: ["**/*.{js,ts}"],
+    plugins: {
+      "license-header": licenseHeaderPlugin,
+    },
+    rules: {
+      "license-header/header": ["error", licenseHeader],
+    },
   },
   {
     ...js.configs.recommended,
